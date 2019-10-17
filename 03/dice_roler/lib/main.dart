08906801c4f9d3a083dfb0b0bcaa0fd8e1928923
AlankritNayak <55492088+AlankritNayak@users.noreply.github.com>
2019-10-17 12:47:19 +0530

    Update main.dart

--- 03/dice_roler/lib/main.dart
+++ 03/dice_roler/lib/main.dart
@@ -10,73 +10,76 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-  var imglist = [ AssetImage("assets/images/dice1.jpg.jpg", 
-              ),
-     AssetImage("assets/images/dice2.jpg.jpg"),
-      AssetImage("assets/images/dice3.jpg.jpg"),
-     AssetImage("assets/images/dice4.jpg.jpg"),
-     AssetImage("assets/images/dice5.jpg.jpg"),
-     AssetImage("assets/images/dice6.jpg.jpg")];
-   
+
+var imglist = [
+  AssetImage(
+    "assets/images/dice1.jpg.jpg",
+  ),
+  AssetImage("assets/images/dice2.jpg.jpg"),
+  AssetImage("assets/images/dice3.jpg.jpg"),
+  AssetImage("assets/images/dice4.jpg.jpg"),
+  AssetImage("assets/images/dice5.jpg.jpg"),
+  AssetImage("assets/images/dice6.jpg.jpg")
+];
+
 class _MyAppState extends State<MyApp> {
- // @override
-   var initImage1;
-   var initImage2;
-   changeDiceNum(){
-     int rand = Random().nextInt(6);
-     int rand2  = Random().nextInt(6);
-     setState(() {
-      initImage1 = imglist[rand];
-      initImage2 = imglist[rand2];
-     });
-   }
+  // @override
+  var initImage1 = imglist[0];
+  var initImage2 = imglist[0];
+
   Widget build(BuildContext context) {
-   
     return MaterialApp(
-     
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Roll Your Dice",
-            textAlign: TextAlign.center,
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.redo),
-              onPressed: () {},
-            )
-          ],
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Roll Your Dice",
+          textAlign: TextAlign.center,
         ),
-        body:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              
-              
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(child:
-                    Image(image:
-                   initImage1),height: 100, width: 100,),
-                   Container(child: Image (image: 
-                    initImage2),height: 100, width: 100,
-                   )
-                  ]),
-              
-              
-              Text(
-                "   "
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.redo),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                child: Image(image: initImage1),
+                height: 100,
+                width: 100,
               ),
-              FlatButton(
-                child: Text("change"),
-                onPressed: () => changeDiceNum(),
-              )
-            ],
-          ),
-        
-        
-      )
-    );
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                child: Image(image: initImage2),
+                height: 100,
+                width: 100,
+              ),
+            )
+          ]),
+          Text("   "),
+          FlatButton(
+            child: Text("change"),
+            onPressed: () {
+              int rand = Random().nextInt(6);
+              int rand2 = Random().nextInt(6);
+              setState(() {
+                initImage1 = imglist[rand];
+                print("inside set state");
+              });
+              print("outside set state");
+              initImage2 = imglist[rand2];
+              print("after image2 update");
+            },
+          )
+        ],
+      ),
+    ));
   }
 }
